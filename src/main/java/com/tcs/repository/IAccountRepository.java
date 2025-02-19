195a640853844696e36b0cfcbc3b070c2a73165c
Angelo-Ponce <angeloponcedev@gmail.com>
2025-02-18 22:19:30 -0500

    add repository

---
+++ src/main/java/com/tcs/repository/IAccountRepository.java
@@ -0,0 +1,6 @@
+package com.tcs.repository;
+
+import com.tcs.model.AccountEntity;
+
+public interface IAccountRepository extends IGenericRepository<AccountEntity, Long> {
+}
---
+++ src/main/java/com/tcs/repository/IMovementRepository.java
@@ -0,0 +1,6 @@
+package com.tcs.repository;
+
+import com.tcs.model.MovementEntity;
+
+public interface IMovementRepository extends IGenericRepository<MovementEntity, Long>{
+}
