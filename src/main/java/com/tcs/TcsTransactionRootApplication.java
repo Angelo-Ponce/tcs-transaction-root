41a0469bde9e23bc3e0b3d0e626ec25e23a126dc
Angelo-Ponce <angeloponcedev@gmail.com>
2025-02-27 12:22:17 -0500

    Add eureka and security

--- src/main/java/com/tcs/TcsTransactionRootApplication.java
+++ src/main/java/com/tcs/TcsTransactionRootApplication.java
@@ -2,11 +2,13 @@ package com.tcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.ReactiveAuditorAware;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import reactor.core.publisher.Mono;
 
+@EnableDiscoveryClient
 @EnableR2dbcAuditing
 @SpringBootApplication
 public class TcsTransactionRootApplication {
