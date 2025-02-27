4f5f66361dca6f22cd51d489553d9c0a4c4a5327
Angelo-Ponce <angeloponcedev@gmail.com>
2025-02-27 17:10:08 -0500

    Add test controller

--- src/test/java/com/tcs/controller/AccountControllerIntegrationTest.java
+++ src/test/java/com/tcs/controller/AccountControllerIntegrationTest.java
@@ -3,69 +3,94 @@ package com.tcs.controller;
 import com.tcs.dto.AccountDTO;
 import com.tcs.model.Account;
 import com.tcs.service.IAccountService;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(AccountController.class)
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@TestPropertySource(properties = "eureka.client.enabled=false")
+@WithMockUser(roles = {"ADMIN"})
 class AccountControllerIntegrationTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @MockitoBean
-    private IAccountService accountService;
+    private IAccountService service;
+
+    @MockitoBean
+    private WebProperties.Resources resources;
 
     private AccountDTO accountDTO;
     private Account account;
 
-//    @BeforeEach
-//    void setUp() {
-//        accountDTO = AccountDTO.builder()
-//                .accountId(1L)
-//                .accountNumber(12345678)
-//                .accountType("SAVINGS")
-//                .initialBalance(BigDecimal.valueOf(1000.0))
-//                .build();
-//
-//        accountEntity = AccountEntity.builder()
-//                .accountId(1L)
-//                .accountNumber(12345678)
-//                .accountType("SAVINGS")
-//                .initialBalance(BigDecimal.valueOf(1000.0))
-//                .createdDate(new Date())
-//                .createdByUser("TestUser")
-//                .build();
-//    }
-//
-//    @Test
-//    @WithMockUser(roles = "ADMIN")
-//    void findAll_ShouldReturnListOfAccounts_WhenHasAdminRole() throws Exception {
-//        when(accountService.findAll()).thenReturn(Arrays.asList(accountEntity));
-//
-//        mockMvc.perform(get("/api/v1/cuentas")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").isArray())
-//                .andExpect(jsonPath("$.data[0].accountNumber").value("12345678"));
-//
-//        verify(accountService, times(1)).findAll();
-//    }
-//
-//    @Test
-//    @WithMockUser(roles = "USER")
-//    void getAccountById_ShouldReturnAccount_WhenIdExists() throws Exception {
-//        when(accountService.findById(1L, "Account")).thenReturn(accountEntity);
-//
-//        mockMvc.perform(get("/api/v1/cuentas/{id}", 1)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data.accountNumber").value("12345678"));
-//
-//        verify(accountService, times(1)).findById(1L, "Account");
-//    }
+    @BeforeEach
+    void setUp() {
+        accountDTO = AccountDTO.builder()
+                .accountId(1L)
+                .accountNumber("12345678")
+                .accountType("SAVINGS")
+                .initialBalance(BigDecimal.valueOf(1000.0))
+                .build();
+
+        account = Account.builder()
+                .accountId(1L)
+                .accountNumber("12345678")
+                .accountType("SAVINGS")
+                .initialBalance(BigDecimal.valueOf(1000.0))
+                .build();
+    }
+
+    @Test
+    //@WithMockUser(roles = "ADMIN")
+    void findAll_ShouldReturnListOfAccounts_WhenHasAdminRole(){
+        when(service.findAll()).thenReturn(Flux.just(account));
+
+        webTestClient.get()
+                .uri("/api/v1/cuentas")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(AccountDTO.class)
+                .hasSize(1)
+                .contains(accountDTO);
+    }
+
+    @Test
+    void givenAccountId_whenFindById_thenReturnAccount() {
+        when(service.findById(1L)).thenReturn(Mono.just(account));
+
+        webTestClient.get()
+                .uri("/api/v1/cuentas/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(AccountDTO.class)
+                .isEqualTo(accountDTO);
+    }
+
+    @Test
+    void givenAccountId_whenFindById_thenReturnNotFound() {
+        when(service.findById(1L)).thenReturn(Mono.empty());
+
+        webTestClient.get()
+                .uri("/api/v1/cuentas/1")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
